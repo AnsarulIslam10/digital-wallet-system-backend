163be25518036184bcae29908298cb3ca985fa8b
AnsarulIslam10 <ansarulislam01739@gmail.com>
2025-08-21 12:42:05 +0600

    logout route

--- src/app/modules/auth/auth.route.ts
+++ src/app/modules/auth/auth.route.ts
@@ -3,4 +3,5 @@ import { AuthControllers } from "./auth.controller";
 
 const router = Router()
 router.post("/login", AuthControllers.credentialsLogin)
+router.post("/logout", AuthControllers.logout)
 export const AuthRoutes = router;
